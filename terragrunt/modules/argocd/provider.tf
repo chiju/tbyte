6f1df8f64d609550c048b895faa0ce48498f9499
chiju <chijumelveettil@gmail.com>
2025-12-13 17:17:50 +0100

    fix: add dedicated providers to ArgoCD module
    - Move Helm/Kubernetes providers into ArgoCD module
    - Use cluster variables directly for authentication
    - Should resolve provider initialization timing issues

---
+++ terragrunt/modules/argocd/provider.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38"
+    }
+  }
+}
+
+# Kubernetes Provider - uses cluster info from variables
+provider "kubernetes" {
+  host                   = var.cluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      var.cluster_name,
+      "--region",
+      var.aws_region
+    ]
+  }
+}
+
+# Helm Provider - uses cluster info from variables  
+provider "helm" {
+  kubernetes = {
+    host                   = var.cluster_endpoint
+    cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+    
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        var.cluster_name,
+        "--region",
+        var.aws_region
+      ]
+    }
+  }
+}
--- terragrunt/modules/argocd/variables.tf
+++ terragrunt/modules/argocd/variables.tf
@@ -65,6 +65,12 @@ variable "aws_region" {
   type        = string
 }
 
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
 variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
