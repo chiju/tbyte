66626de17fa1eb5ff86f61ed6cba43a24eeb4b0f
chiju <chijumelveettil@gmail.com>
2025-12-13 13:52:23 +0100

    fix: add missing aws_region and assume_role_arn variables to IAM module
    - Add aws_region variable with default value eu-central-1
    - Add assume_role_arn variable with default null value
    - These variables are required by the provider configuration

--- terragrunt/modules/iam/variables.tf
+++ terragrunt/modules/iam/variables.tf
@@ -1,3 +1,15 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "assume_role_arn" {
+  description = "ARN of the role to assume for cross-account access"
+  type        = string
+  default     = null
+}
+
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
