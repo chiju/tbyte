47a38c418bf65bfc4cbe2aa384adb6f93edb7c89
chiju <chijumelveettil@gmail.com>
2025-12-13 11:41:52 +0100

    fix: correct shared-services outputs to match ECR module outputs

--- terragrunt/modules/shared-services/outputs.tf
+++ terragrunt/modules/shared-services/outputs.tf
@@ -1,7 +1,19 @@
-output "frontend_ecr_url" {
-  value = module.ecr.frontend_ecr_url
+output "frontend_repository_url" {
+  description = "URL of the frontend ECR repository"
+  value       = module.ecr.frontend_repository_url
 }
 
-output "backend_ecr_url" {
-  value = module.ecr.backend_ecr_url
+output "backend_repository_url" {
+  description = "URL of the backend ECR repository"
+  value       = module.ecr.backend_repository_url
+}
+
+output "frontend_repository_name" {
+  description = "Name of the frontend ECR repository"
+  value       = module.ecr.frontend_repository_name
+}
+
+output "backend_repository_name" {
+  description = "Name of the backend ECR repository"
+  value       = module.ecr.backend_repository_name
 }
